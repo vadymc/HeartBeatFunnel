ba1dae2016f9d8c1327202af938d26b06e721aca
vadymc <vadym.chekrii@transferwise.com>
2018-11-18 08:13:30 +0200

    project rename

--- src/test/kotlin/vadc/heartbeat/HeartBeatFunnelApplicationTests.kt
+++ src/test/kotlin/vadc/heartbeat/HeartBeatFunnelApplicationTests.kt
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
-class HeartBeatPublisherApplicationTests {
+class HeartBeatFunnelApplicationTests {
 
 	@Test
 	fun contextLoads() {
