eefc6fd1b783127bdacbc146754fce43a126eb04
vadymc <vadym.chekrii@transferwise.com>
2018-11-18 16:13:04 +0200

    deleted gradle release from travis

--- scripts/travis_build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "${TRAVIS_BRANCH}" = "master" ]; then
-    echo "Building master"
-    ./gradlew release -Prelease.useAutomaticVersion=true
-else
-    echo "Building branch"
-    ./gradlew check
-fi
